61f9ee75cd29cc65fcfafe60f85e8ea4183528bf
manemis <manemis@gmail.com>
2022-01-27 14:24:18 +0400

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -10,13 +10,23 @@ document.querySelector(`.guess`).value = 23;
 console.log(document.querySelector(`.guess`).value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
-document.querySelector(`.number`).textContent = secretNumber;
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.score`).textContent = score;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.guess`).value = '';
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
+});
+
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
-  console.log(guess, typeof guess);
 
   // when there's no input
   if (!guess) {
@@ -25,25 +35,19 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     // when player wins
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `🎉 Correct Number!`;
+    document.querySelector(`.number`).textContent = secretNumber;
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
     document.querySelector(`.number`).style.width = `30rem`;
-    // when guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      score--; // decrease by 1
-      document.querySelector(`.score`).textContent = score;
-      document.querySelector(`.message`).textContent = `⬇️ Lower than that`;
-    } else {
-      document.querySelector(`.score`).textContent = 0;
-      document.querySelector(`.message`).textContent = `😔 Oh You lost`;
+    if (score > highScore) {
+      highScore = score;
     }
-
-    // when guess is too low
-  } else if (guess < secretNumber) {
+    document.querySelector(`.highscore`).textContent = highScore;
+  } else if (guess !== secretNumber) {
     if (score > 1) {
       score--; // decrease by 1
       document.querySelector(`.score`).textContent = score;
-      document.querySelector(`.message`).textContent = `⬆️ Higher than that`;
+      document.querySelector(`.message`).textContent =
+        guess > secretNumber ? `⬇️ Lower than that` : `⬆️ Higher than that`;
     } else {
       document.querySelector(`.score`).textContent = 0;
       document.querySelector(`.message`).textContent = `😔 Oh You lost`;
